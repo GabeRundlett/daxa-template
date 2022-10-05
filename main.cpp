cdd6d718fa92e83188e09ac1ed0d6d0d290b3b51
GabeRundlett <gabe@rundlett.com>
2022-10-05 09:47:29 -0600

    Update Daxa registry and make build (+ add new CMake)

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@
 #define APPNAME "Daxa Template App"
 #define APPNAME_PREFIX(x) ("[" APPNAME "] " x)
 
-#define DAXA_GLSL 0
-#define DAXA_HLSL 1
+#define DAXA_GLSL 1
+#define DAXA_HLSL 0
 
 #include <daxa/utils/imgui.hpp>
 #include <imgui_impl_glfw.h>
@@ -41,7 +41,7 @@ struct App : AppWindow<App>
         },
         .present_mode = daxa::PresentMode::DO_NOT_WAIT_FOR_VBLANK,
         .image_usage = daxa::ImageUsageFlagBits::TRANSFER_DST,
-        .debug_name = APPNAME_PREFIX("swpachain"),
+        .debug_name = APPNAME_PREFIX("swapchain"),
     });
 
     daxa::PipelineCompiler pipeline_compiler = device.create_pipeline_compiler({
@@ -70,16 +70,6 @@ struct App : AppWindow<App>
             .format = swapchain.get_format(),
         });
     }
-
-    daxa::BinarySemaphore binary_semaphore = device.create_binary_semaphore({
-        .debug_name = APPNAME_PREFIX("binary_semaphore"),
-    });
-    static inline constexpr u64 FRAMES_IN_FLIGHT = 1;
-    daxa::TimelineSemaphore gpu_framecount_timeline_sema = device.create_timeline_semaphore(daxa::TimelineSemaphoreInfo{
-        .initial_value = 0,
-        .debug_name = APPNAME_PREFIX("gpu_framecount_timeline_sema"),
-    });
-    u64 cpu_framecount = FRAMES_IN_FLIGHT - 1;
 
     Clock::time_point start = Clock::now(), prev_time = start;
     f32 elapsed_s = 1.0f;
@@ -115,8 +105,6 @@ struct App : AppWindow<App>
     daxa::TaskBufferId task_gpu_input_buffer;
     daxa::TaskBufferId task_staging_gpu_input_buffer;
 
-    daxa::BinarySemaphore acquire_semaphore = device.create_binary_semaphore({.debug_name = APPNAME_PREFIX("acquire_semaphore")});
-    daxa::BinarySemaphore present_semaphore = device.create_binary_semaphore({.debug_name = APPNAME_PREFIX("present_semaphore")});
     daxa::CommandSubmitInfo submit_info;
 
     daxa::ImageId render_image = device.create_image(daxa::ImageInfo{
@@ -163,6 +151,18 @@ struct App : AppWindow<App>
         return false;
     }
 
+    auto reload_pipeline(auto &pipeline) -> bool {
+        if (pipeline_compiler.check_if_sources_changed(pipeline)) {
+            auto new_pipeline = pipeline_compiler.recreate_compute_pipeline(pipeline);
+            std::cout << new_pipeline.to_string() << std::endl;
+            if (new_pipeline.is_ok()) {
+                pipeline = new_pipeline.value();
+                return true;
+            }
+        }
+        return false;
+    }
+
     void on_update()
     {
         auto now = Clock::now();
@@ -174,25 +174,12 @@ struct App : AppWindow<App>
 
         ui_update();
 
-        if (pipeline_compiler.check_if_sources_changed(compute_pipeline))
-        {
-            auto new_pipeline = pipeline_compiler.recreate_compute_pipeline(compute_pipeline);
-            if (new_pipeline.is_ok())
-            {
-                compute_pipeline = new_pipeline.value();
-                std::cout << "Compilation succeeded" << std::endl;
-            }
-            else
-            {
-                std::cout << new_pipeline.message() << std::endl;
-            }
-        }
+        reload_pipeline(compute_pipeline);
 
-        swapchain_image = swapchain.acquire_next_image(acquire_semaphore);
-        ++cpu_framecount;
-        submit_info.signal_timeline_semaphores = {{gpu_framecount_timeline_sema, cpu_framecount}};
+        swapchain_image = swapchain.acquire_next_image();
+        if (swapchain_image.is_empty())
+            return;
         loop_task_list.execute();
-        gpu_framecount_timeline_sema.wait_for_value(cpu_framecount - 1);
     }
 
     void on_mouse_move(f32 x, f32 y)
@@ -248,6 +235,7 @@ struct App : AppWindow<App>
     {
         daxa::TaskList new_task_list = daxa::TaskList({
             .device = device,
+            .dont_use_split_barriers = true,
             .swapchain = swapchain,
             .debug_name = APPNAME_PREFIX("task_list"),
         });
